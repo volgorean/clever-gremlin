0588db2a76d39cabc42a57c4fc371601540b28e7
Peter <volgorean@gmail.com>
2014-04-22 17:41:21 -0700

    scrapes website, parses out all words, sorts by frequency

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,12 +1,21 @@
 class Blog < ActiveRecord::Base
+  require 'open-uri'
   validates :url, presence: true
 
   def webScrape(url)
     begin
-      require 'open-uri'
       doc = Nokogiri::HTML(open(url))
+      text  = doc.at('body').inner_text
+      words = text.scan(/[a-z]+/i)
     rescue
       return false
     end
   end
+
+  # returns an array of words by their useage in an array ..eg.. [["blabla", 6],["hai", 2],["lolwut", 1]]
+  def wordCount(words)
+    words.group_by(&:downcase).map{ |word,instances|
+      [word,instances.length]
+    }.sort_by(&:last).reverse
+end
 end
